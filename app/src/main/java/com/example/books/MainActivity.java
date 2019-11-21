294f26799fc875c8a6f848ccdecb76c0e23ce0a3
ykakran05 <ykakran05@gmail.com>
2019-11-21 10:33:51 +0530

    java and xml of main activity

---
+++ app/src/main/java/com/example/books/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.books;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void move(View view)
+    {
+        Intent i =new Intent(this,Main2Activity.class);
+
+        startActivity(i);
+    }
+}
